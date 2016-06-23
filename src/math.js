a52ea6d884b3032b5c59a5f84d70af43cba378d1
Glenn Spinner <glenngineer1@gmail.com>
2016-06-23 11:18:03 -0500

    basic functions written for all math examples

--- src/math.js
+++ src/math.js
@@ -6,4 +6,18 @@ function subtract(num1, num2) {
   return num1 - num2;
 }
 
-function multiply
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function square(num1, num2) {
+  return Math.pow(num1, 2);
+}
+
+function squareRoot(num1) {
+  return Math.sqrt(num1);
+}
